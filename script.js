19e04b492f176b57f4d13e8340e8f10b02be91b5
Asubek <asubekerakyluly@gmail.com>
2025-02-13 11:50:07 +0500

    tataattatat

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const text = document.getElementById('text');
 const length = document.getElementById('length');
+let textLength = 0 
 
 text.addEventListener('input', () => {
-    length.textContent = text.value.length
+    if (text.value == "") {
+        textLength += 0
+    }
+    else{
+        for (let i = 0; i < text.value.length; i++) {
+            
+            i++
+            
+        }
+    }
+    
+    length.textContent = textLength
 })
