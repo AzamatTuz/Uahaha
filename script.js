b8f3357a339ef0132eb921771a1e26e942da8020
Azamat <tuzelbajazamat361@gmail.com>
2025-02-13 11:49:04 +0500

    Changes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const text = document.getElementById('text');
 const length = document.getElementById('length');
+let textLength = 0;
 
 text.addEventListener('input', () => {
-    length.textContent = text.value.length
-})
+
+    length.textContent = `Text Symbols: ${text.value.trim()}` 
+
+});
