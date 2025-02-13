09e658fd3fc288c6a9e2b87e046a4244b137c0e1
Azamat <tuzelbajazamat361@gmail.com>
2025-02-13 12:21:10 +0500

    Changes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const text = document.getElementById('text');
 const length = document.getElementById('length');
-let textLength = 0;
+const words = document.getElementById('words');
 
 text.addEventListener('input', () => {
 
-    length.textContent = `Text Symbols: ${text.value.trim()}` 
-
+    words.textContent = `Text Words: ${WordCount(text.value.trim())}`
+    length.textContent = `Text Symbols: ${text.value.trim().length}`
+    
 });
+
+function WordCount(str) {
+    return str.split(" ").length;
+}
+
+console.log(("str1,str2,str3,str4".match(/ /g) || []).length)
